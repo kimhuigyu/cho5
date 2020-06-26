0aa5e0853df136dd97f4f9dc700aa21daec32637
HuiGyu.KiM <gmlrb2091@cu.ac.kr>
2020-06-26 12:26:21 +0900

    소스 추가

---
+++ cho5/test.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main() {
+	int i = 10, j = 20;
+	int *ptr;
+	printf("\n i의 값 = %d \n j의 값 = %d", i, j);
+	printf("\n i의 메모리 주소(&i) = %u", &i);
+	printf("\n j의 메모리 주소(&j) = %u", &j);
+
+
+
+
+}
